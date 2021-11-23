5ef13be2bc4f4dcee28e04e5a1412258c9a68369
Toropolski_Bartosz <toropolski.bartosz@gmail.com>
2021-11-23 17:11:30 +0100

    first commit

---
+++ src/main/java/com/toropolski/Socialnetworkingservice/SocialNetworkingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toropolski.Socialnetworkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkingServiceApplication.class, args);
+	}
+
+}
